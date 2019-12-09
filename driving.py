afba2f282d7cb3e2c6e96b67231bb8440060fa48
AliceLiu <usa821026@gmail.com>
2019-12-09 13:03:06 +0800

    add US Part

--- driving.py
+++ driving.py
@@ -6,3 +6,8 @@ if country == '台灣':   ##要是國籍是台灣 如果不是那就部會印出
     print('你可以考駕照')
   else: 
     print('你還不能考駕照')
+elif country == '美國':   ##要是國籍是美國 如果不是那就部會印出任何東西
+  if age >= 16:
+    print('你可以考駕照')
+  else: 
+    print('你還不能考駕照')
