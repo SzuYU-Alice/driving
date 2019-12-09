48c383d3e565c5463a1b0d51d200a089c002af60
AliceLiu <usa821026@gmail.com>
2019-12-09 12:50:23 +0800

    add Taiwan

---
+++ driving.py
@@ -0,0 +1,8 @@
+country = input('請問你是哪國人：')
+age = input('請輸入年齡：')
+age = int(age)
+if country == '台灣':   ##要是國籍是台灣 如果不是那就部會印出任何東西
+  if age >= 18:
+    print('你可以考駕照')
+  else: 
+    print('你還不能考駕照')
